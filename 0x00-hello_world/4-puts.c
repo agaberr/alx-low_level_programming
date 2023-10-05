7bca0e8f9034d122d69dd855c003301ca53e0aa8
root <root@DESKTOP-G4BMPVI>
2023-10-05 15:44:18 +0300

    Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-int main(){
+int main(void)
+{
+
+/*
+ * main - Entry point
+ *
+ * Description: 'Prints Programming is like building a multilingual puzzle using puts'
+ *
+ * Return: Always 0 (Success)
+ */
 
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
