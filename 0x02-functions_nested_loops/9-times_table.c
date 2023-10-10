401418b2dd3aff15192fa817195a8f653e8c56b6
root <root@DESKTOP-G4BMPVI>
2023-10-10 21:19:02 +0300

    Tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,13 +15,13 @@ void times_table(void)
 
 	int i = 0, j = 0;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			printf("%2d, ", (i * j));
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 
 }
