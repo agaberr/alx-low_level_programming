b9a1a38ce6b74f340e66e0266c4174f3deff7234
root <root@DESKTOP-G4BMPVI>
2023-10-10 20:33:55 +0300

    Tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * Description: return the last digit of a number
+ * @n: the number given
+ *
+ * Return: the last digit of a number
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: prints every minute of day starting
+ * from 00:00 to 23:59
+ *
+ *
+ * Return: the last digit of a number
+*/
+
+void jack_bauer(void)
+{
+
+	int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+
+	while (n1 != 2 && n2 != 4 && n3 = 0 && n4 = 0)
+	{
+
+		_putchar(n1 + '0');
+		_putchar(n2 + '0');
+		_putchar(':');
+		_putchar(n3 + '0');
+		_putchar(n4 + '0');
+		_putchar('\n');
+
+		n4++;
+
+		if (n4 == 10)
+		{
+			n4 = 0;
+			n3++;
+		}
+		if (n3 == 6)
+		{
+			n3 = 0;
+			n2++;
+		}
+		if (n2 == 10)
+		{
+			n2 = 0;
+			n1++;
+		}
+	}
+
+}
