c0bcca87718e627e27a75915aadfaefeb5e7e230
root <root@DESKTOP-G4BMPVI>
2023-10-10 11:52:58 +0300

    Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Print _putchar, followed
+ * by a new line
+ *
+ * Return: Always 0 (success)
+*/
+
+
+int main(void)
+{
+
+	char word[] = "_putchar";
+	int wordSize = sizeof(word) / sizeof(word[0]);
+
+	int i = 0;
+
+	while (i < wordSize)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
