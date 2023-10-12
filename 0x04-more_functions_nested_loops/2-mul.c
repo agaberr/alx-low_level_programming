dce3950d7c1420abdd1b998e9165954b00aa2d44
root <root@DESKTOP-G4BMPVI>
2023-10-12 19:18:25 +0300

    Tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: multiply 2 integers
+ * @a: first integet
+ * @b: second integer
+ *
+ * Return: multiplication result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: print from 0 to 9
+ *
+ * Return: None
+*/
+
+void print_numbers(void)
+{
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
