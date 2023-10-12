5d24d0d81d08e25d5b929572a75b6ed744869fcf
root <root@DESKTOP-G4BMPVI>
2023-10-12 19:28:53 +0300

    Tasks

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,14 @@ void more_numbers(void)
 
 	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 14; j++)
 		{
-			if (i > 9)
+			if (j > 9)
 			{
 				_putchar('1');
-				_putchar((i - 10) + '0');
+				_putchar((j - 10) + '0');
 			}
 			else
 			{
@@ -29,5 +29,4 @@ void more_numbers(void)
 		}
 	}
 	_putchar('\n');
-
 }
