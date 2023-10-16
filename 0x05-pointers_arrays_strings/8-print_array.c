8ec6923c86b3be329c7f16f0955629243dbc2e8f
root <root@DESKTOP-G4BMPVI>
2023-10-16 15:55:26 +0300

    Tasks

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,8 +14,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d, ", a[i]);
+		if (i + 1 >= n)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
 	}
-        printf("%d\n", a[i]);
 
 }
