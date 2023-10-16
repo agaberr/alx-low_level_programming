337f2f4039e8bd381fb8b715c4f74de74bcddd29
root <root@DESKTOP-G4BMPVI>
2023-10-16 15:20:55 +0300

    Tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,12 @@ void puts2(char *str)
 
 	int i = 0;
 
-	while (str[i] != '\0' && str[i - 1] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i += 2;
+		if (str[i - 1] != '\0')
+			break;
 	}
 	_putchar('\n');
 
