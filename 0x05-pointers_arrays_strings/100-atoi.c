86ebdbbb7a5662d9f7e537d2454c2439ae8fb348
root <root@DESKTOP-G4BMPVI>
2023-10-16 16:21:33 +0300

    Tasks

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string into int
+ * @s: string to be converted
+ *
+ * Return: integer after converstion
+ */
+
+int _atoi(char *s)
+{
+
+	int i = 0, negFlag = 0, val = 0;
+	int len = strlen(s);
+
+	/* check if  empty */
+	if (len == 0)
+		return (0);
+
+	/* check for negetive */
+	if (s[0] == '-')
+	{
+		negFlag = 1;
+		len--;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+
+		if (nrgFlag == 1)
+			i++;
+
+		val += s[i] * (10 * (len - 1))
+
+	}
+
+	return (val);
+
+
+}
