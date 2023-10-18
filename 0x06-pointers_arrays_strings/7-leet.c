552c9681f66ee99535de101c82d87fa049bfdc63
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-18 23:13:44 +0300

    Tasks

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,21 +10,19 @@
 char *leet(char *str)
 {
 
-	int i;
+	int i, j;
+
+	char letter[] = {'a', 'e', 'o', 't', 'l'};
+
+	char val[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; str[i]; i++)
 	{
-
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == letter[j] || str[i] == (letter[j] - 32))
+				str[i] = val[j];
+		}
 
 	}
 
