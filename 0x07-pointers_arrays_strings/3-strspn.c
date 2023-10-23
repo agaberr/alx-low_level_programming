f5ddb2b5bf08cba0cf716f915ccc6cf6ea975c84
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-23 15:25:20 +0300

    Tasks

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,17 +13,17 @@
 unsigned int _strspn(char *s, char *accept)
 {
 
-	int count = 1;
-	int i, j;
+	unsigned int i = 0;
 
-	for (i = 0; accept[i]; i++)
-		for (j = 0; s[j] && j != 1000; j++)
-			if (accept[i] == s[j])
-			{
-				count++;
-				j = 999;
-			}
+	while (*s != '\0')
+	{
+		if (!strchr(accept, *s))
+			break;
 
-	return (count);
+		i++;
+		s++;
+	}
+
+	return (i);
 
 }
