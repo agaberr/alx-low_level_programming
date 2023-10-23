266def776fd13135eaf2b01178dc6bd44d407d6d
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-23 15:48:59 +0300

    Tasks

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@
 char *_strchr(char *s, char c)
 {
 
-	unsigned int i;
-
-	for (i = 0; s[i]; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (s + i);
+		if (*s == c)
+			return (s);
+		s++;
 	}
+
 	return (NULL);
 
 }
