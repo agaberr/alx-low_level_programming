2f0568ee73b62338ab2199c1aae0737e60f18736
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-25 23:16:02 +0300

    Tasks

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* wildcmp - compares two strings
+* @s1: first string
+* @s2: second string
+*
+* Return: 1 if identical, 0 otherwise
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	else if (*s1 == *s2 || *s2 == '*')
+	{
+
+		if (*s2 == '*')
+			if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
+				return (1);
+
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	}
+
+	else
+		return (0);
+
+}
