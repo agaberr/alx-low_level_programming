0e47ab235bf308e9ee1047e2e8995c91edd4d073
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-25 23:32:20 +0300

    Tasks

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,6 +14,10 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
+	else if (s1[strlen(s1 - 1)] != s1[strlen(s1 - 1)]
+	&& s1[strlen(s1 - 1)] != '*')
+		return (0);
+
 	else if (*s1 == *s2 || *s2 == '*')
 	{
 
