f23adb27cc67d9a765535824e9ba4124391294c7
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-25 15:33:23 +0300

    Tasks

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,7 +7,23 @@
 * Return: None;
 */
 
+void print_no_newline(char *s);
 void _print_rev_recursion(char *s)
+{
+
+	print_no_newline(*s);
+	_putchar('\n');
+
+}
+
+/**
+* print_no_newline - prints string reversed followed
+* @s: string to print
+*
+* Return: None;
+*/
+
+void print_no_newline(char *s)
 {
 
 	if (*s == '\0')
@@ -21,3 +37,5 @@ void _print_rev_recursion(char *s)
 	}
 
 }
+
+
