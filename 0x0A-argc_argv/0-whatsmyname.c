a9ea193daf5d30f6174534cd93969ac3277a2c35
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-27 13:06:35 +0200

    Tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: number of parameters
+* @argv: array of parameters
+*
+* Return: Always 0 (success)
+*/
+
+int main(int argc, int *argv[])
+{
+
+	printf("%s", argv[0]);
+	return (0);
+
+}
