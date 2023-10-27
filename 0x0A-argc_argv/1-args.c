84e313bbc649f6d0146cbd297fef21439b2b9b29
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-27 13:11:12 +0200

    Tasks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+
+}
