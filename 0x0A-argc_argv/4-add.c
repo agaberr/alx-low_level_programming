2468f54359f007736a2e5df512e4abd081138074
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-10-27 13:46:27 +0200

    Tasks

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 if success, 1 otherwise
+*/
+
+int main(int argc, char *argv[])
+{
+
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+
+		if (!isdigit(argv[i][0]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+
+}
