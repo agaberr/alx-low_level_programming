30a5227bc47c0b8fa85cb43fc26acdf6958c2b4a
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-07 00:43:19 +0200

    Tasks

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+* init_dog - initialize dog struct with values
+* @d: dog struct
+* @name: dog name
+* @age: dog age
+* @owner: dog owner name
+*
+* Return: None
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
