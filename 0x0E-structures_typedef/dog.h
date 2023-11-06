acb476f2fd20466f8b0e141e64c226b36f3238c0
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-07 00:38:45 +0200

    Tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - struct of type dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ *
+ */
+
+struct dog
+{
 
 	char *name;
 	float age;
