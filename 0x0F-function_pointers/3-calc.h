3f7d536571c422065b4bcd38ee32b04a4f99d9e6
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-09 01:34:21 +0200

    Tasks

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,29 @@
+#ifndef _3_CALC_H_
+#define _3_CALC_H_
+
+/**
+* struct op - the struct of op
+*
+* @op: operator
+* @f: function
+*/
+typedef struct op
+{
+	char *op;
+
+	int (*f)(int a, int b);
+
+} op_t;
+
+/* Function prototypes */
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
+
+#endif
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+* get_op_func - get the right function
+* @s: operator passed to program
+* Return: pointer to function of operator
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
