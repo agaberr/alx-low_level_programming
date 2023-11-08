a160edc74cccb80a4635365241b6e329e755ddec
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-09 00:54:38 +0200

    Tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,23 +1,18 @@
 #include "function_pointers.h"
 
 /**
-* print_name: Calls a function that prints name
+* print_name - Calls a function that prints name
 * @name: string to print
 * @f: function to call
 *
 * Return: None
 */
 
-
-
 void print_name(char *name, void (*f)(char *))
 {
 	void (*ptr)(char (*));
 
-	if (ptr != NULL && name != NULL)
-	{
-		ptr = f;
-		ptr(name);
-	}
+	ptr = f;
+	ptr(name);
 
 }
