75cd504991338e5e9145f78645dc6b564dbae9bd
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-09 19:06:32 +0200

    Tasks

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - return sum of all its parameters
+* @n: number of params
+*
+* Return: Sum of all parameters
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+	unsigned int i, sum = 0;
+
+	va_list ptr;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ptr, int);
+
+	va_end(ptr);
+
+	return (sum);
+
+}
