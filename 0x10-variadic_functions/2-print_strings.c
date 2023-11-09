edf5cba33d1bb60622ce46427067a2beb340e813
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-09 23:13:48 +0200

    Tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -32,7 +32,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str)
 			printf("%s", str);
 		else
-			printf("(nill)");
+			printf("(nil)");
 
 		if (separator != NULL)
 		printf("%s", separator);
