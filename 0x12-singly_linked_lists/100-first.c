d5277c42040e49fa8455a91ec959bcb8d13398c3
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-16 17:42:44 +0200

    Tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+* print_before_main - print string before main
+*
+* Return: None
+*/
+
+
+void print_before_main(void) __attribute__((constructor));
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
