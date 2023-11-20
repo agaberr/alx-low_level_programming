1796f206154d794496d3818d16037d01bb88a3ff
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-20 18:23:00 +0200

    Tasks

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *temp_node;
 
 
-	if (head == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp_node2;
 
 
-	if (head == NULL || *head == NULL)
+	if (new_node == NULL || *head == NULL)
 		return (NULL);
 
 	new_node->n = n;
