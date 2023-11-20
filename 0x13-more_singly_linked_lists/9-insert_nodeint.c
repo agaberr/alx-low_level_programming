93dcc0cf40299775e09d17ffacde4d6d7049f2cf
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-20 21:31:36 +0200

    Tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,49 @@
 #include "lists.h"
 /**
-* insert_nodeint_at_index - adds a new node at a given position
+* insert_nodeint_at_index - adds a new_node node at a given position
 * @head: head of linkedlist
-* @idx: index of the list where the new node should be added. Index starts at 0
+* @idx: index of the list where the new_node node
+* should be added. Index starts at 0
 * @n: value to add to node
 *
-* Return: address of the new element, or NULL if it failed
+* Return: address of the new_node element, or NULL if it failed
 */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 
 	unsigned int i;
+	listint_t *new_node, *temp_node = *head;
 
-	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *temp_node1;
-	listint_t *temp_node2;
+	new_node = malloc(sizeof(listint_t));
 
-
-	if (new_node == NULL || *head == NULL || head == NULL)
+	if (new_node == NULL || head == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	temp_node1 = *head;
-	temp_node2 = new_node;
+	new_node->next = NULL;
 
-	for (i = 0; i < idx - 1; i++)
+	if (idx == 0)
 	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-		if (temp_node1 == NULL)
+	for (i = 0; i < idx; i++)
+	{
+		if (temp_node)
 		{
-			free(new_node);
-			return (NULL);
+			if (i + 1 == idx)
+			{
+				new_node->next = temp_node->next;
+				temp_node->next = new_node;
+				return (new_node);
+			}
+			else
+				temp_node = temp_node->next;
 		}
-		temp_node1 = temp_node1->next;
 	}
 
-	if (temp_node1 == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	temp_node2->next = temp_node1->next;
-	temp_node1->next = temp_node2;
-
-	return (new_node);
-
+	return (NULL);
 }
