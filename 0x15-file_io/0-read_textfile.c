430f72aefba00cf9bf6be4d00dd50306ff0ddc26
Ahmed Gaber <ahmedgaber312001@gmail.com>
2023-11-27 22:45:37 +0200

    Tasks

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+* read_textfile - read a text file and prints it to the POSIX standard output
+* @filename: file name provided
+* @letters: number of letters to print
+*
+* Return: the actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	if (filename == NULL)
+		return (0);
+
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+		return (0);
+
+	char *buffer = malloc(letters + 1);
+
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	ssize_t bytes = fread(buffer, 1, letters, file);
+
+	if (bytes < 0)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+
+	buffer[bytes] = '\0';
+
+	ssize_t w_bytes = fwrite(buffer, 1, letters, stdout);
+
+	if (w_bytes < 0)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+	free(buffer);
+	fclose(file);
+	return (bytes);
+}
