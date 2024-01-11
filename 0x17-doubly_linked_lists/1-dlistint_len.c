dbda429fc16607e8b51cb2e4d88e43e7f86a3007
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-11 21:00:43 +0200

    tasks

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* dlistint_len - returns the number of elements
+* @h: header to double linked list
+*
+* Return: the number of nodes
+*
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int counter = 0;
+
+	if (!h)
+	{
+		return (0);
+	}
+
+	while (h)
+	{
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+
+}
